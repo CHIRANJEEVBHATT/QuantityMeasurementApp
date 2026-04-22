80715fe9ebedcd1999a9df78e4be88c203f79132
Chiranjeev Bhatt <chiranjeevbhatt18@gmail.com>
2026-04-22 09:30:10 +0530

    UC5

--- Test/QuantityMeasurementAppTest.java
+++ Test/QuantityMeasurementAppTest.java
@@ -3,27 +3,55 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class QuantityMeasurementAppTest {
 
+    double EPS = 1e-6;
+
     @Test
-    void testFeetToFeet_SameValue() {
-        assertTrue(new QuantityLength(1.0, LengthUnit.FEET)
-                .equals(new QuantityLength(1.0, LengthUnit.FEET)));
+    void testFeetToInches() {
+        assertEquals(12.0, QuantityLength.convert(1.0, LengthUnit.FEET, LengthUnit.INCH), EPS);
     }
 
     @Test
-    void testInchToFeet_Equivalent() {
-        assertTrue(new QuantityLength(12.0, LengthUnit.INCH)
-                .equals(new QuantityLength(1.0, LengthUnit.FEET)));
+    void testInchesToFeet() {
+        assertEquals(2.0, QuantityLength.convert(24.0, LengthUnit.INCH, LengthUnit.FEET), EPS);
     }
 
     @Test
-    void testDifferentValues() {
-        assertFalse(new QuantityLength(1.0, LengthUnit.FEET)
-                .equals(new QuantityLength(2.0, LengthUnit.FEET)));
+    void testYardToInch() {
+        assertEquals(36.0, QuantityLength.convert(1.0, LengthUnit.YARD, LengthUnit.INCH), EPS);
     }
 
     @Test
-    void testNullComparison() {
-        QuantityLength q = new QuantityLength(1.0, LengthUnit.FEET);
-        assertFalse(q.equals(null));
+    void testCmToInch() {
+        assertEquals(1.0, QuantityLength.convert(2.54, LengthUnit.CM, LengthUnit.INCH), 1e-3);
+    }
+
+    @Test
+    void testRoundTrip() {
+        double val = 5.0;
+        double converted = QuantityLength.convert(val, LengthUnit.FEET, LengthUnit.INCH);
+        double back = QuantityLength.convert(converted, LengthUnit.INCH, LengthUnit.FEET);
+        assertEquals(val, back, EPS);
+    }
+
+    @Test
+    void testZero() {
+        assertEquals(0.0, QuantityLength.convert(0.0, LengthUnit.FEET, LengthUnit.INCH), EPS);
+    }
+
+    @Test
+    void testNegative() {
+        assertEquals(-12.0, QuantityLength.convert(-1.0, LengthUnit.FEET, LengthUnit.INCH), EPS);
+    }
+
+    @Test
+    void testInvalidUnit() {
+        assertThrows(IllegalArgumentException.class, () ->
+                QuantityLength.convert(1.0, null, LengthUnit.FEET));
+    }
+
+    @Test
+    void testInvalidValue() {
+        assertThrows(IllegalArgumentException.class, () ->
+                QuantityLength.convert(Double.NaN, LengthUnit.FEET, LengthUnit.INCH));
     }
 }
